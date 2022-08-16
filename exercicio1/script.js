3b184b9b8edaa4c119cd8e15cb86876cef508ba8
izabela08 <izabelalima17@gmail.com>
2022-08-16 20:36:22 -0300

    d-izabela-lima-a015L

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,9 +1,20 @@
-let coxinha = prompt('Deseja uma coxinha? sim [S] / não [N] : ')
-let conta = 0
+// PRIMEIRO COD
+// let coxinha = prompt('Deseja uma coxinha? sim [S] / não [N] : ')
+// let conta = 0
 
-while(coxinha == "s"){
-    conta = conta + 2.50
-    coxinha = prompt('Deseja mais uma coxinha? [S]sim / [N]não')
+// while(coxinha == "s"){
+//     conta = conta + 2.50
+//     coxinha = prompt('Deseja mais uma coxinha? [S]sim / [N]não')
+// }
+// alert('Sua conta é de: ' + conta)
 
+// SEUNGA TENTATIVA
+let question = prompt('Deseja comer mais coxinhas?\n Sim = S\n Não = N').toUpperCase()
+let count = 0
+
+while (question !== 'N' || question === 'S') {
+    question === 'S' ? count += 2.5 : console.log('Dígito inválido.')
+    question = prompt('Deseja comer mais coxinhas?\n Sim = S\n Não = N').toUpperCase()
 }
-alert('Sua conta é de ' + conta)
+
+console.log(`O valor total foi de R$${count}`)
